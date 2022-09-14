2c42830fdb4883ab3f253f937f8a552ed8de272f
Habiballah Heazrehee <hahh20@student.bth.se>
2022-09-14 11:06:03 +0200

    done with lab 2

---
+++ Labs/lab1.py
@@ -0,0 +1,10 @@
+def lab_one_message():
+    print("Den här labborationen har reda lämnats in i form av 2 länkar till mina repo på Github.")
+
+
+def main():
+    lab_one_message()
+
+
+if __name__ == "__main__":
+    main()
